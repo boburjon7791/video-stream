dfefe83fa1b915c270118f3a3ec7e344967abee9
Boburjon <soliyevboburjon95@gmail.com>
2024-12-26 23:15:50 +0500

    write log

--- src/main/java/com/example/demo/MyInterceptor.java
+++ src/main/java/com/example/demo/MyInterceptor.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,13 +13,17 @@ import java.util.Iterator;
 
 @Configuration
 public class MyInterceptor implements HandlerInterceptor, WebMvcConfigurer {
+    private static final Logger log = LoggerFactory.getLogger(MyInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Iterator<String> iterator = request.getHeaderNames().asIterator();
+        log.info("request headers start");
         while (iterator.hasNext()) {
             String headerName = iterator.next();
-            System.out.println(headerName +": " + request.getHeader(headerName));
+            log.info("{}: {}", headerName, request.getHeader(headerName));
         }
+        log.info("request headers end");
         return true;
     }
 
