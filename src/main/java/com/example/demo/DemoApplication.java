e7c9a2a4c263c056e0f12feff9835cf35e8edcdc
Boburjon <soliyevboburjon95@gmail.com>
2024-12-26 23:11:57 +0500

    second commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,27 +1,33 @@
-package com.example;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-        return application.sources(DemoApplication.class);
-    }
+public class DemoApplication {
+    String rootFolder="G:/Videoder";
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-    @RequestMapping(value="/")
-    public String hello() {
-        return "Hello World from Tomcat";
+    @GetMapping(value = "/video/{fileName}", produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> video(@PathVariable String fileName) throws IOException {
+        String path=rootFolder+"/"+fileName;
+        File file=new File(path);
+        UrlResource resource = UrlResource.from(file.toURI());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+resource.getFilename()+"\"")
+                .header(HttpHeaders.ACCEPT_RANGES,"bytes")
+                .body(resource);
     }
-
-
 }
--- src/main/java/com/example/demo/MyInterceptor.java
+++ src/main/java/com/example/demo/MyInterceptor.java
@@ -1,2 +1,28 @@
-package com.example.demo;public class MyInterceptor {
+package com.example.demo;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Iterator;
+
+@Configuration
+public class MyInterceptor implements HandlerInterceptor, WebMvcConfigurer {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Iterator<String> iterator = request.getHeaderNames().asIterator();
+        while (iterator.hasNext()) {
+            String headerName = iterator.next();
+            System.out.println(headerName +": " + request.getHeader(headerName));
+        }
+        return true;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(this);
+    }
 }
